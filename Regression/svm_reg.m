4a0fc0ab4f5c3bac60ada8a719007cc5c364a1ed
JiachennCJC <jiachenn99@gmail.com>
2020-12-14 22:57:40 +0000

    Messy code, messy variables, should work

--- Regression/svm_reg.m
+++ Regression/svm_reg.m
@@ -7,46 +7,27 @@ data_subset = data_combined(1:2000, :);
 [x,y] = size(data_combined);
 
 %% Specify predictor and response variables (Train test features x labels)
-% predictorTrain = data_combined(1:floor(size(data_combined,1))/5*4,1:y-1);
-% responseTrain = data_combined(1:floor(size(data_combined,1))/5*4,y);
-% predictorTest = data_combined(floor(size(data_combined,1)/5*4)+1:x ,1:y-1);
-% responseTest = data_combined(floor(size(data_combined,1)/5*4)+1:x ,y);
+predictorTrain = data_combined(1:floor(size(data_combined,1))/5*4,1:y-1);
+responseTrain = data_combined(1:floor(size(data_combined,1))/5*4,y);
 
-predictorTrain = data_subset(1:1500,1:y-1);
-responseTrain = data_subset(1:1500,y);
+predictorTest = data_combined(floor(size(data_combined,1)/5*4)+1:x ,1:y-1);
+responseTest = data_combined(floor(size(data_combined,1)/5*4)+1:x ,y);
 
-predictorTest = data_subset(1500:size(data_subset,1),1:y-1);
-responseTest = data_subset(1500:size(data_subset,1),y);
+%% Linear kernel training (commented out to save time for now)
+% Mdl = fitrsvm(predictorTrain, responseTrain,'KernelFunction', 'linear' ,'Epsilon', 0.5, 'Standardize', true);
+% 
+% % Test on test set 
+% acc = predict(Mdl, predictorTest);
+% 
+% % Calculate MSE of the predicted set against GT
+% mse = immse(table2array(responseTest), acc);
+% rmse = sqrt(mse);
+% 
+% disp("Convergence: " + Mdl.ConvergenceInfo.Converged)
+% disp(" # of support vectors: " + size(Mdl.SupportVectors,1))
+% disp("RMSE: " + rmse)
 
-%% Linear kernel training
-Mdl = fitrsvm(predictorTrain, responseTrain,'KernelFunction', 'linear' ,'Epsilon', 0.5, 'Standardize', true);
-
-% Test on test set 
-acc = predict(Mdl, predictorTest);
-
-% Calculate MSE of the predicted set against GT
-mse = immse(table2array(responseTest), acc);
-rmse = sqrt(mse);
-
-disp("Convergence: " + Mdl.ConvergenceInfo.Converged)
-disp(" # of support vectors: " + size(Mdl.SupportVectors,1))
-disp("RMSE: " + rmse)
-
-%% TO WRITE CV FOR HYPERPARAMS FINDINGS
-
-% Hyperparameters declaration
-boxConstraints = [0.1, 1, 5, 10, 20];
-kernelFunctions = ["linear", "rbf", "polynomial"];
-kernelScale = [0.1, 1, 5, 10, 20];
-polynomialOrder = 2:4;
-epsilonScale = [0.3, 0.4, 0.5, 0.6, 0.7];
-
-% Store results
-resultsNestedCV.linear = zeros(4, 1, length(boxConstraints));
-
-% number of SV, ratio of SV, accuracy, points for both
-resultsNestedCV.rbf = zeros(4, length(kernelScale), length(boxConstraints));    
-resultsNestedCV.polynomial = zeros(4, length(polynomialOrder), length(boxConstraints));
+%% Part (b) Train each model -> Linear, RBF, Polynomial
 
 % Reduce number of datapoints for CV
 predictorCV = predictorTrain(1:height(predictorTrain)/10, :);
@@ -54,26 +35,34 @@ responseCV = responseTrain(1:height(responseTrain)/10, :);
 
 cvFolds = 5;
 
-for f = kernelFunctions
-    count = 1;
+% Hyperparameters declaration for regression
+boxConstraints = [0.1, 1, 5, 10, 20];
+epsilonScale = [0.3, 0.5, 0.7, 0.9];
+
+% Store results
+% resultsNestedCV.linear = zeros(3, 1, length(boxConstraints));
+
+%% RBF Kernel
+kernelScale = [0.1, 1, 5, 10, 20];
+
+resultsNestedCV.rbf = zeros(3, 1, length(boxConstraints));    
+count = 1;
+for c = boxConstraints
+    bestBestBestRMSE = 1;
+    bestBestBestModel = 0;
+    bestEpsilon = 0;
     
-    for c = boxConstraints
-        if f == "linear"        
-            argsVals = 1:1 ;
-            
-        elseif f == "rbf"
-            argsName = "KernelScale";
-            argsVals = kernelScale;
-            
-        elseif f == "polynomial"
-            argsName = "PolynomialOrder";
-            argsVals = polynomialOrder;       
-        end
+    for ep = epsilonScale
+        fprintf("\n")
+        disp("Current ep: "+ep)
         
-        for val = argsVals
-            highestOuterPoint = 0;
-            bestOuterModel = 0;
-            bestAcc = 0;
+        bestKernelScale = 0;
+        bestBestRMSE = 1;
+        bestBestModel = 0;
+        
+        for k = kernelScale
+            bestRMSE = 1;
+            bestKernelModel = 0;
             
             for outerFold = 1:cvFolds
                 % split dataset into training and testing datasets in each fold
@@ -81,14 +70,14 @@ for f = kernelFunctions
                 featuresFoldTrain1 = predictorCV(1:(outerFold-1)*(floor(size(predictorCV,1)/10)), :);
                 featuresFoldTrain2 = predictorCV(outerFold*(floor(size(predictorCV,1)/10))+1:size(predictorCV,1), :);
                 featuresFoldTrainOuter = [featuresFoldTrain1; featuresFoldTrain2];
-                
+
                 labelsFoldTestOuter = responseCV((outerFold-1)*(floor(size(predictorCV,1)/10))+1:outerFold*(floor(size(predictorCV,1)/10)), :);
                 labelsFoldTrain1 = responseCV(1:(outerFold-1)*(floor(size(predictorCV,1)/10)), :);
                 labelsFoldTrain2 = responseCV(outerFold*(floor(size(predictorCV,1)/10))+1:size(responseCV,1), :);
                 labelsFoldTrainOuter = [labelsFoldTrain1; labelsFoldTrain2];
                 
-                highestInnerPoint = 0;
-                bestInnerModel = 0;
+                lowestInnerFoldRMSE = 1;
+                bestInnerFoldModel = 0;
                 
                 for innerFold = 1:cvFolds
                     % split dataset into training and testing datasets in each fold
@@ -96,42 +85,141 @@ for f = kernelFunctions
                     featuresFoldTrain1 = featuresFoldTrainOuter(1:(innerFold-1)*(floor(size(featuresFoldTrainOuter,1)/10)), :);
                     featuresFoldTrain2 = featuresFoldTrainOuter(innerFold*(floor(size(featuresFoldTrainOuter,1)/10))+1:size(featuresFoldTrainOuter,1), :);
                     featuresFoldTrainInner = [featuresFoldTrain1; featuresFoldTrain2];
-                    
+
                     labelsFoldTestInner = labelsFoldTrainOuter((innerFold-1)*(floor(size(featuresFoldTrainOuter,1)/10))+1:innerFold*(floor(size(featuresFoldTrainOuter,1)/10)), :);
                     labelsFoldTrain1 = labelsFoldTrainOuter(1:(innerFold-1)*(floor(size(featuresFoldTrainOuter,1)/10)), :);
                     labelsFoldTrain2 = labelsFoldTrainOuter(innerFold*(floor(size(featuresFoldTrainOuter,1)/10))+1:size(labelsFoldTrainOuter,1), :);
                     labelsFoldTrainInner = [labelsFoldTrain1; labelsFoldTrain2];
+                
+                    % Train model
+                    modelRegression = fitrsvm(featuresFoldTrainInner, labelsFoldTrainInner, 'KernelFunction', 'rbf', 'BoxConstraint',c, 'Epsilon',ep);
                     
-                     % train SVM
-                    if f == "linear"
-                        modelRegression = fitrsvm(featuresFoldTrainInner, labelsFoldTrainInner, 'KernelFunction',f, 'BoxConstraint',c);
-                    else
-                        modelRegression = fitrsvm(featuresFoldTrainInner, labelsFoldTrainInner, 'KernelFunction',f, 'BoxConstraint',c, funcArgName,val);
-                    end
-                    
-                    % Evaluate SVM on fold test set 
-                    acc = predict(modelRegression, featuresFoldTestInner);
-                    
-                    % Calculate MSE of the predicted set against GT
-                    mse = immse(table2array(labelsFoldTestInner), acc);
+                    % Evaluate the model
+                    predicted_labels = predict(modelRegression, featuresFoldTestInner);
+                    mse = immse(table2array(labelsFoldTestInner), predicted_labels);
                     rmse = sqrt(mse);
                     
-                    disp("Inner fold: " + innerFold);
-                    disp("Outer fold: " + outerFold);
-                    disp("Kernel Function: " + f);
-                    disp("Box Constraints: " + c);
-                    disp("Num of support vectors: " + size(modelRegression.SupportVectors,1))
+                    if rmse < lowestInnerFoldRMSE
+                        lowestInnerFoldRMSE = rmse;
+                        bestInnerFoldModel = modelRegression;
+                    end
+                end
+                
+                % Evaluate the best inner fold model
+                predicted_labels = predict(bestInnerFoldModel, featuresFoldTestOuter);
+                mse = immse(table2array(labelsFoldTestOuter), predicted_labels);
+                rmse = sqrt(mse);
+                
+                if rmse < bestRMSE
+                    bestRMSE = rmse;
+                    bestModel = bestInnerFoldModel;
                 end  
-            end    
-        end
+            end
+            disp("BestRMSE: "+bestRMSE)
+            disp("BestBestRMSE: "+bestBestRMSE)
+           
+            if bestRMSE < bestBestRMSE
+                bestKernelScale = k;
+                bestBestRMSE = bestRMSE;
+                bestBestModel = bestModel;
+                disp("New best kernel scale: "+bestKernelScale)
+            end
+            fprintf("\n")
+        end  
     end
+    if bestBestRMSE < bestBestBestRMSE
+        bestBestBestRMSE = bestBestRMSE;
+        bestEpsilon = ep;
+        bestBestBestModel = bestBestModel;
+    end
+    disp("Triple best RMSE: "+bestBestBestRMSE)
+    
+    suppVecNum = size(bestBestBestModel.SupportVectors,1);
+    suppVecRat = suppVecNum/height(featuresFoldTrainOuter) * 100;
+    
+    resultsNestedCV.rbf(count) = suppVecNum;
+    resultsNestedCV.rbf(count+1) = suppVecRat;
+    resultsNestedCV.rbf(count+2) = bestBestBestRMSE;
+    
+    count = count+3;
+    
 end
-
-
-% Gaussian kernel
-
-
-% RBF kernel 
-
-
-% Polynomial kernel
+%% Polynomial Kernel
+% polynomialOrder = 2:4;
+% resultsNestedCV.polynomial = zeros(5, length(polynomialOrder), length(boxConstraints));
+% 
+% for c = boxConstraints
+%     for ep = epsilonScale
+%         fprintf("\n")
+%         disp("Current ep: "+ep)
+%         
+%         bestPolynomialOrder = 0;
+%         bestBestRMSE = 1;
+%         
+%         for k = polynomialOrder
+%             bestRMSE = 1;
+%             
+%             for outerFold = 1:cvFolds
+%                 % split dataset into training and testing datasets in each fold
+%                 featuresFoldTestOuter = predictorCV((outerFold-1)*(floor(size(predictorCV,1)/10))+1:outerFold*(floor(size(predictorCV,1)/10)), :);
+%                 featuresFoldTrain1 = predictorCV(1:(outerFold-1)*(floor(size(predictorCV,1)/10)), :);
+%                 featuresFoldTrain2 = predictorCV(outerFold*(floor(size(predictorCV,1)/10))+1:size(predictorCV,1), :);
+%                 featuresFoldTrainOuter = [featuresFoldTrain1; featuresFoldTrain2];
+% 
+%                 labelsFoldTestOuter = responseCV((outerFold-1)*(floor(size(predictorCV,1)/10))+1:outerFold*(floor(size(predictorCV,1)/10)), :);
+%                 labelsFoldTrain1 = responseCV(1:(outerFold-1)*(floor(size(predictorCV,1)/10)), :);
+%                 labelsFoldTrain2 = responseCV(outerFold*(floor(size(predictorCV,1)/10))+1:size(responseCV,1), :);
+%                 labelsFoldTrainOuter = [labelsFoldTrain1; labelsFoldTrain2];
+%                 
+%                 lowestInnerFoldRMSE = 1;
+%                 bestInnerFoldModel = 0;
+%                 
+%                 for innerFold = 1:cvFolds
+%                     % split dataset into training and testing datasets in each fold
+%                     featuresFoldTestInner = featuresFoldTrainOuter((innerFold-1)*(floor(size(featuresFoldTrainOuter,1)/10))+1:innerFold*(floor(size(featuresFoldTrainOuter,1)/10)), :);
+%                     featuresFoldTrain1 = featuresFoldTrainOuter(1:(innerFold-1)*(floor(size(featuresFoldTrainOuter,1)/10)), :);
+%                     featuresFoldTrain2 = featuresFoldTrainOuter(innerFold*(floor(size(featuresFoldTrainOuter,1)/10))+1:size(featuresFoldTrainOuter,1), :);
+%                     featuresFoldTrainInner = [featuresFoldTrain1; featuresFoldTrain2];
+% 
+%                     labelsFoldTestInner = labelsFoldTrainOuter((innerFold-1)*(floor(size(featuresFoldTrainOuter,1)/10))+1:innerFold*(floor(size(featuresFoldTrainOuter,1)/10)), :);
+%                     labelsFoldTrain1 = labelsFoldTrainOuter(1:(innerFold-1)*(floor(size(featuresFoldTrainOuter,1)/10)), :);
+%                     labelsFoldTrain2 = labelsFoldTrainOuter(innerFold*(floor(size(featuresFoldTrainOuter,1)/10))+1:size(labelsFoldTrainOuter,1), :);
+%                     labelsFoldTrainInner = [labelsFoldTrain1; labelsFoldTrain2];
+%                 
+%                     % Train model
+%                     modelRegression = fitrsvm(featuresFoldTrainInner, labelsFoldTrainInner, 'KernelFunction', 'rbf', 'BoxConstraint',c, 'Epsilon',ep);
+%                     
+%                     % Evaluate the model
+%                     predicted_labels = predict(modelRegression, featuresFoldTestInner);
+%                     mse = immse(table2array(labelsFoldTestInner), predicted_labels);
+%                     rmse = sqrt(mse);
+%                     
+%                     if rmse < lowestInnerFoldRMSE
+%                         lowestInnerFoldRMSE = rmse;
+%                         bestInnerFoldModel = modelRegression;
+%                     
+%                     end
+%                 end
+%                 
+%                 % Evaluate the best inner fold model
+%                 predicted_labels = predict(bestInnerFoldModel, featuresFoldTestOuter);
+%                 mse = immse(table2array(labelsFoldTestOuter), predicted_labels);
+%                 rmse = sqrt(mse);
+%                 
+%                 if rmse < bestRMSE
+%                     bestRMSE = rmse;
+%                 end  
+%             end
+%             
+%             disp("BestRMSE: "+bestRMSE)
+%             disp("BestBestRMSE: "+bestBestRMSE)
+%            
+%             if bestRMSE < bestBestRMSE
+%                 bestPolynomialOrder = k;
+%                 bestBestRMSE = bestRMSE;
+%                 disp("New best kernel scale: "+bestPolynomialOrder)
+%             end
+%             fprintf("\n")
+%         end 
+%     end
+% end
